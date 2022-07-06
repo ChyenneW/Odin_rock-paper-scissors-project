903e9eecbf1e9e91c15c28ea409abdb91a118d76
ChyenneW <williamscheyenne771@gmail.com>
2022-07-06 16:57:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,26 +5,26 @@ const possibleChoices = document.querySelectorAll('button');
 const computerScoreDisplay = document.getElementById('computer-score');
 const userScoreDispay = document.getElementById('player-score');
 
-let userChoice
+let playersChoice;
 
 // Randomly Return either Rock Paper or Scissors
 function computerPlay() {
   // define three options or one object? one array?
   let playOptions = ["rock", "paper", "scissors"];
   // randomly return number between 1-3
-  let playChoice = playOptions[Math.floor(Math.random() * 3)]
+  let computerChoice = playOptions[Math.floor(Math.random() * 3)]
   // return computer's choice
-  return playChoice;
+  return computerChoice;
 }
 
 function playersPlay() {
   possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-    userChoice = e.target.id
-    userChoiceDispay.innerHTML = userChoice
+    playersChoice = e.target.id
+    userChoiceDispay.innerHTML = playersChoice
     // Displays the results from computerPlay on the html
     computerChoiceDisplay.innerHTML = computerPlay()
-
-    let playersChoice = userChoice
-    return playersChoice
   }))
+  return playersChoice;
 }
+
+playersPlay();
