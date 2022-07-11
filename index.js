8f587a5af7b4ad13f58dbf457288131446af4472
ChyenneW <williamscheyenne771@gmail.com>
2022-07-11 11:09:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const possibleChoices = document.querySelectorAll('button');
 const computerScoreDisplay = document.getElementById('computer-score');
 const userScoreDispay = document.getElementById('player-score');
 
+let computerChoice;
 let playersChoice;
 
 // Randomly Return either Rock Paper or Scissors
@@ -14,17 +15,48 @@ function computerPlay() {
   // randomly return number between 1-3
   let computerChoice = playOptions[Math.floor(Math.random() * 3)]
   // return computer's choice
+  console.log(computerChoice);
   return computerChoice;
 }
 
 function playersPlay() {
   possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     playersChoice = e.target.id
-    userChoiceDispay.innerHTML = playersChoice
+    userChoiceDispay.innerHTML = playersChoice;
     // Displays the results from computerPlay on the html
-    computerChoiceDisplay.innerHTML = computerPlay()
+    computerChoiceDisplay.innerHTML = computerPlay();
+    result.innerHTML = gameResults(computerChoice, playersChoice);
   }))
-  return playersChoice;
+
+}
+
+
+
+// Play round between computer and player
+function gameResults(computerChoice, playersChoice) {
+  // take computerPlay and playersPlay
+  let computer = computerChoice;
+  let player = playersChoice;
+  console.log(`choice is ${computer}, ${player}`);
+  // let victor = ["computerWon", "playerWon", "tie"];
+  console.log(computer);
+  console.log(player);
+
+  // Compare computerPlay and playersPlay
+  if (computer === undefined || player === undefined) {
+    return result.innerHTML = " ";
+  } else if (computer === player) {
+    return result.innerHTML = "Its a tie!!";
+    // gameScore(victor[2]);
+
+  } else if (computer == "rock" && player == "scissors" || computer == "paper" && player == "rock" || computer == "scissors" && player == "paper") {
+    return result.innerHTML = `You lose! ${computer} beats ${player}.`;
+    // gameScore(victor[0]);
+
+  } else {
+    return result.innerHTML = `You win! ${player} beats ${computer}.`;
+    // gameScore(victor[1]);
+  }
 }
 
 playersPlay();
